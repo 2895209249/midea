17e289b20d5f0d8e4af7b7aab80f3f482e57bd85
2895209249 <2895209249@qq.com>
2020-07-04 11:30:58 +0800

    v 1.0.6

--- src/js/magnifier.js
+++ src/js/magnifier.js
@@ -7,12 +7,6 @@ function magnifier() {
     li = $('.thumbnails>li>img')
     picimg = document.querySelector('.smallpic>img')
     bigimg = document.querySelector('.bigpic')
-        // console.log(bigpic);
-        // console.log(smallpic);
-        // console.log(big);
-        // console.log(bigpic);
-        // console.log(smallpic);
-        // console.log(movebox);
 
     // 1. 绑定事件
     smallpic.on('mouseover', function() {
@@ -21,32 +15,33 @@ function magnifier() {
         big.addClass('show');
         // movebox的大小计算
         movebox.css({
-            width: (smallpic.offset().left * big.offset().left) / bigpic.offset().left + 'px' ,
-            height: (smallpic.offset().top * big.offset().top) / bigpic.offset().top + 'px' ,
+            width: ((smallpic.width() * big.width()) / bigpic.width()  )/2+ 'px',
+            height: ((smallpic.height() * big.height()) / bigpic.height() )/2+ 'px' 
         })
         // 2.让movebox跟随鼠标移动
         smallpic.on('mousemove', function(ev) {
-            // console.log(smallpic.offset().top);
+            // console.log(smallpic.height());
 
-            let top = ev.pageY - smallpic.offset().top - movebox.offset().top / 2;
-            let left = ev.pageX - smallpic.offset().left - movebox.offset().left / 2;
+            let top = ev.pageY - smallpic.offset().top - movebox.height() / 2;
+            let left = ev.pageX - smallpic.offset().left - movebox.width() / 2;
 
             // 3.比例计算
-            let ratio = bigpic.offset().left / smallpic.offset().top; // 小数 大于1的数
-            let uratio = bigpic.offset().top / smallpic.offset().left;
-            // console.log(bigpic.offset().top / smallpic.offset().top);
-
+            let ratio = bigpic.width() / smallpic.height(); // 小数 大于1的数
+            let uratio = bigpic.height()/ smallpic.width();
+            // console.log(bigpic.height() / smallpic.height());
+            console.log(top);
+            console.log(left);
             // 边界管理
             if (top <= 0) {
                 top = 0;
-            } else if (top >= smallpic.offset().height - movebox.offset().height) {
-                top = smallpic.offset().height - movebox.offset().height - 2;
+            } else if (top >= smallpic.height() - movebox.height()) {
+                top = smallpic.height() - movebox.height() - 2;
             }
 
             if (left <= 0) {
                 left = 0;
-            } else if (left >= smallpic.offset().width - movebox.offset().width) {
-                left = smallpic.offset().width - movebox.offset().width - 2;
+            } else if (left >= smallpic.width() - movebox.width()) {
+                left = smallpic.width() - movebox.width() - 2;
             }
 
             movebox.css({
